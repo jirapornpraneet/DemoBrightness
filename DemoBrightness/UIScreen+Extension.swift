acb687c9fe56268bcfda093757c00ea4506fb472
Donut <>
2021-09-29 11:49:53 +0700

    edit code

--- DemoBrightness/UIScreen+Extension.swift
+++ DemoBrightness/UIScreen+Extension.swift
@@ -9,15 +9,6 @@ import Foundation
 import UIKit
 
 extension UIScreen {
-    static func setMainBrightness(brightness: CGFloat) {
-        guard (0...1).contains(brightness) else {
-            print("Attempt to set the screen brightness to an invalid value: \(brightness) should be between 0 and 1 inclusive.")
-            return
-        }
-        print("Brightness\(brightness)")
-        self.main.brightness = brightness
-    }
-    
     private static let step: CGFloat = 0.1
     
     static func animateBrightness(to value: CGFloat) {
